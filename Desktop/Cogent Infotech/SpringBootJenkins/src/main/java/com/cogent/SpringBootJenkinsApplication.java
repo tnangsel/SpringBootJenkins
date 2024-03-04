e9194f0483352c01b01e3579b15b9c64bb830f18
tnangsel <79672177+tnangsel01@users.noreply.github.com>
2024-03-04 16:16:53 -0600

    Jenkins

---
+++ Desktop/Cogent Infotech/SpringBootJenkins/src/main/java/com/cogent/SpringBootJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.cogent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+	 static Logger log = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	 @PostConstruct
+	 void init() {
+		 log.debug("This is init constructor statement");
+	 }
+	 
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
---
+++ Desktop/Cogent Infotech/SpringBootJenkins/src/test/java/com/cogent/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cogent;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	static Logger log = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
